70296c395f3caaa4ab3b2761efed36af43de6f4b
Vitalii Sydorenko <vetal.sydo@gmail.com>
2018-06-30 16:12:54 +0300

    added search module

--- storage-search/src/test/java/com/storage/storagesearch/StorageSearchApplicationTests.java
+++ storage-search/src/test/java/com/storage/storagesearch/StorageSearchApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zero.index.root;
+package com.storage.storagesearch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RootApplicationTests {
+public class StorageSearchApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
